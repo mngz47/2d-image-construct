5f8083407440bb18e5ff79923d38e7dd3d95e26e
Matthew <mngz636@gmail.com>
2026-01-25 23:16:31 +0200

    Update api.js

--- api.js
+++ api.js
@@ -7,20 +7,17 @@ canvas.ondblclick = function(){
       if(e("x_axis").value){
           if(e("y_axis").value){
             
-    var vv =  ne("label");
-
-    vv.innerHTML = "O"
-    vv.style.position = "relative";
-    vv.style.left = e("x_axis").value;
-    vv.style.top = e("y_axis").value;
-            
-    canvas.appendChild(vv);
+   putNewVertex(canvas,e("x_axis").value,e("y_axis").value);
 
       // The script must sync with the canvas to reflect the same image
 
-      e("object_script_txt").value =
+    if(e("object_script_txt").value==""){
+ e("object_script_txt").value =
             "Define 2d object" +
 "Object name" + " " + e("object_name").value; 
+    }
+
+  e("object_script_txt").value +=   
 "Vertex fields\n"+
 "x_axis" + " " +  e("x_axis").value+
 "y_axis" + " " +  e("y_axis").value
@@ -39,6 +36,19 @@ alert("insert object name");
   e("y_axis").value = event.clientY;
    
 });
+}
+
+function putNewVertex(canvas,x,y){
+
+ var vv =  ne("label");
+
+    vv.innerHTML = "O"
+    vv.style.position = "relative";
+    vv.style.left = x;
+    vv.style.top = y-150;
+            
+    canvas.appendChild(vv);
+  
 }
 
  getNewVertex(e("2d-canvas"));
