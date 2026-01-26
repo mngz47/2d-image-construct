3fedba9b2caa5040c640e62643a54505d3be6606
Matthew <mngz636@gmail.com>
2026-01-26 16:03:01 +0200

    Update api.js

--- api.js
+++ api.js
@@ -128,8 +128,8 @@ function putNewVertex(canvas,x,y){
 
     vv.innerHTML = "O"
     vv.style.position = "relative";
-    vv.style.left = x;
-    vv.style.top = y;
+    vv.style.left = x+"px";
+    vv.style.top = y+"px";
             
     canvas.appendChild(vv);
   
